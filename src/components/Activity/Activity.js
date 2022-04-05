fd5627f3cadfeb5629938120c8cbdefb655a6835
asrafaliii <asraf.bookaand@gmail.com>
2022-04-05 11:30:57 +0600

    added fontawesome icon

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Activity.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Activity = ({activity}) => {
     const {name, picture, rating, comment } = activity;
@@ -9,7 +11,7 @@ const Activity = ({activity}) => {
                 <img src={picture} alt="" />
                 <h4>{name}</h4>
             </div>
-            <h3>{rating}</h3>
+            <h4 className='text-danger mt-2'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon><FontAwesomeIcon icon={faStar}></FontAwesomeIcon><FontAwesomeIcon icon={faStar}></FontAwesomeIcon><FontAwesomeIcon icon={faStar}></FontAwesomeIcon><FontAwesomeIcon icon={faStar}></FontAwesomeIcon>{rating}</h4>
             <p>{comment}</p>
         </div>
     );
