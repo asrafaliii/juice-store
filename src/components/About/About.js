78f4fc6c73d3caf034d59673f6c35391f2532649
asrafaliii <asraf.bookaand@gmail.com>
2022-04-05 11:55:32 +0600

    added about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='container'>
+            <h2 className='text-center'>About</h2>
+            <h6>Juice is a drink made from the extraction or pressing of the natural liquid contained in fruit and vegetables. It can also refer to liquids that are flavored with concentrate or other biological food sources, such as meat or seafood, such as clam juice.</h6>
+            <p>Juice is a drink made from the extraction or pressing of the natural liquid contained in fruit and vegetables. It can also refer to liquids that are flavored with concentrate or other biological food sources, such as meat or seafood, such as clam juice</p>
         </div>
     );
 };
