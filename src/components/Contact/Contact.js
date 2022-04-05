0c5e9e82218b17409ecc9d80d736812618e4ee72
asrafaliii <asraf.bookaand@gmail.com>
2022-04-05 12:00:51 +0600

    contact us

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='container'>
+            <h3 className='text-center mt-2'>Contact Us</h3>
+            <h5>Level-4, 34, Awal Centre, Banani, Dhaka</h5>
+            <h6>Official: web@programming-hero.com</h6>
+            <h6> Helpline : 01322810873 , 01322810867 , 01322810869</h6>
+            <p>(Available : Sat - Thu, 10:00 AM to 7:00 PM)</p>
         </div>
     );
 };
